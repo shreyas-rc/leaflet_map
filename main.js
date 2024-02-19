79cdf9417b35c1bb73fa0d8466fb3d923db52ac9
shreyas-rc <1616shreyas@gmail.com>
2024-02-19 18:24:56 +0530

    Initial commit

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function init(){
         center: [20, 78],
         zoom: 5,
         layers: [
-            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png',{
+            L.tileLayer('http://mt0.google.com/vt/lyrs=y&hl=en&x={x}&y={y}&z={z}',{
                 attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             })
         ]
