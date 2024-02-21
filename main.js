4928f3828bc037d676f3f23d091261d4c3ebb975
shreyas-rc <1616shreyas@gmail.com>
2024-02-21 22:37:11 +0530

    2nd commit

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 window.onload = init;
 
 function init(){
+    
     const mymap = L.map('mapid', {
         center: [20, 78],
         zoom: 5,
         layers: [
             L.tileLayer('http://mt0.google.com/vt/lyrs=y&hl=en&x={x}&y={y}&z={z}',{
-                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                attribution: '<a href="https://www.google.com/maps">Google Maps Hybrid</a>'
             })
         ]
     })
